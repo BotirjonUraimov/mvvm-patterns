fdc71bec31e6d915faf2acc5071e4dfad30936b6
BotirjonUraimov <botirjon.uraimov.93@gmail.com.com>
2024-04-19 21:51:07 +0900

    Fix: unnecessaris

--- app/src/main/java/com/example/mvvm_java_android/login/LoginActivity.java
+++ app/src/main/java/com/example/mvvm_java_android/login/LoginActivity.java
@@ -11,14 +11,11 @@ import com.example.mvvm_java_android.base.BaseActivity;
 import com.example.mvvm_java_android.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements  LoginContract.View{
-
-
     private LoginContract.ActionListener userActionListener;
     @Override
     public ActivityLoginBinding inflateViewBinding(LayoutInflater inflater) {
         return ActivityLoginBinding.inflate(inflater);
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,18 +24,15 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements
         binding.passwordEdittext.addTextChangedListener(userActionListener.registerPasswordWatcher());
         binding.emailEditText.addTextChangedListener(userActionListener.registerTextWatcher());
     }
-
     @Override
     public void moveToMainPage() {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
     }
-
     @Override
     public void showErrorMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
-
     @Override
     public void setValidEmail(boolean isValidEmail) {
         binding.icVerifiedEmail.setVisibility(isValidEmail? View.VISIBLE : View.INVISIBLE);
